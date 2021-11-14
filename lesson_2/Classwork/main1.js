b51823be03c69549b775f95b56a6a5d30db972aa
RomanBuryy <roman.buriy@gmail.com>
2021-11-14 13:08:22 +0200

    Add classwork tasks

--- lesson_2/Classwork/main1.js
+++ lesson_2/Classwork/main1.js
@@ -1,86 +1,102 @@
-//1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
-let a = Math.floor(Math.random() * 10);
-let b = Math.floor(Math.random() * 10);
-let c = Math.floor(Math.random() * 10);
-let d = Math.floor(Math.random() * 10);
-let e = Math.floor(Math.random() * 10);
-let f = Math.floor(Math.random() * 10);
-let j = Math.floor(Math.random() * 10);
-let i = Math.floor(Math.random() * 10);
-let k = Math.floor(Math.random() * 10);
-let l = Math.floor(Math.random() * 10);
+/*
+ Користувач вводить або має два числа.
+    Потрібно знайти та вивести максимальне число з тих двох .
+    Також потрібно врахувати коли введені рівні числа.*/
 
-let arr = [a, b, c, d, e, f, j, i, k, l];
-console.log(arr);
-let result = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9];
-console.log(`Result = ${result}`);
+let a = 10;
+let b = 321;
 
-// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
-let book = {
-    bookName: "Kolobok",
-    pages: 456,
-    bookType: "novel",
+if (a < b) {
+    console.log(b);
+} else {
+    console.log(a);
 }
-console.log(book);
 
-// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
-let book2 = {
-    bookName: "Якась назва",
-    pages: 23,
-    bookType: "novel",
-    authors: ["Braun", "King", "Chapek"],
+/*
+- У першому під'їзді квартири з 1 до 20. У другому з 21 по 48. У третьому з 49 по 90.
+Користувач вводить номер квартири просто в змінні або через prompt('') .
+    Програма повинна зазначити, в якому під'їзді знаходиться дана квартира.
+*/
+let flatNumber = 21;
+if (flatNumber <= 20 && flatNumber > 0) {
+    console.log("Перший підїзд");
+} else if (flatNumber > 20 && flatNumber <= 48) {
+    console.log("Другий підїзд");
+} else if (flatNumber > 48 && flatNumber <= 90) {
+    console.log("Третій підїзд");
 }
-console.log(book2);
 
-// 4. Створити маcив об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
-let books = [{
-    bookName: "Рукавичка",
-    pages: 45,
-    bookType: "novel",
-    authors: ["Braun", "King", "Chapek"]
-},
-    {
-        bookName: "Kolobok",
-        pages: 234,
-        bookType: "novel",
-        authors: ["Braun", "King", "Chapek"]
-    },
-    {
-        bookName: "Джерело",
-        pages: 500,
-        bookType: "novel",
-        authors: ["Braun", "King", "Chapek"]
-    },
-    {
-        bookName: "Воно",
-        pages: 1000,
-        bookType: "novel",
-        authors: ["Braun", "King", "Chapek"],
+// - Ми маємо змінну number в яку користувач задає числове значення,  якщо змінна рівна 10 вивести повідомлення ВІРНО, якщо змінна не рівна 10 - тоді НЕВІРНО
+let number = 1;
+if (number === 10) {
+    console.log("Вірно")
+} else console.log("Невірно");
+
+/*
+- Ми маємо змінну x в яку користувач задає  значення,  якщо в змінну записане число, вивести 1,
+    якщо в змінну записали стрінгу вивести 2, якщо булеве значення - 3, якщо об'єкт або масив - 4.
+якщо в змінну записали щось інше, спрацьовує else*/
+
+let x = 1;
+
+if (typeof (x) === 'number' || typeof (x) === 'string' || typeof (x) === 'object' || typeof (x) === 'boolean') {
+    if (typeof (x) === 'number') {
+        console.log(1);
+    } else if (typeof (x) === 'string') {
+        console.log(2);
+    } else if (typeof (x) === 'boolean') {
+        console.log(3);
+    } else if (typeof (x) === 'object') {
+        console.log(4)
     }
-];
-console.log(books[0]);
-console.log(books[1]);
-console.log(books[2]);
+} else console.log("Error");
 
-// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
-// Значення площі зберігати в змінній s.
-let height = 23;
-let width = 10;
-let area = height * width;
-console.log(area); //230
+/*- Задача-гра "чи ми йдемо сьогодні в OKTEN на навчання?".
+    Змінна, яка характеризує температуру. Якщо температура від +10 до +22 вивести повідомлення що ми йдемо ВЧИТИСЯ .
+    Якщо інша темпаретура, тоді виводимо повідомлення що сидимо вдома і вчимося ОНЛАЙН*/
 
-// Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
-// результат помістіть у змінну v.
-let heightC = 10;
-let dC = 4;
-const pi = 3.14;
-let v = (pi * (dC / 2) ** 2) * heightC;
-console.log(v); //125.6
+let temp = 22;
+if (temp >= 10 && temp <= 22) {
+    console.log("ми йдемо ВЧИТИСЯ");
+} else console.log("сидимо вдома і вчимося ОНЛАЙН");
 
-// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
-// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
-let n = 3;
-let m = 4;
-let q = Math.sqrt( n ** 2 + m ** 2);
-console.log(q); //5
+/*- За допомогою switch case - реалізувати гру - вікторину, де ми вводимо число від 1 до 5. І взалежності від введеного
+    числа нам дається приз. (Авто, Мото, Телефон, і т.д ) і якщо число не підходить тоді  вивести Повідомлення - що число не вірне ... .*/
+
+let prise = 6;
+
+switch (prise) {
+    case 1:
+        console.log("Авто");
+        break;
+    case 2:
+        console.log("Мото");
+        break;
+    case 3:
+        console.log("Телефон");
+        break;
+    case 4:
+        console.log("Літак");
+        break;
+    case 5:
+        console.log("Стіл");
+        break;
+    default:
+        console.log("Incorrect number");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
