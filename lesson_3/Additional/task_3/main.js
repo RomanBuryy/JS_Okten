6104267bac45860dd246f1f40b0c5c077512a2aa
RomanBuryy <roman.buriy@gmail.com>
2021-11-17 08:04:23 +0200

    Add addtional task_2

--- lesson_3/Additional/task_3/main.js
+++ lesson_3/Additional/task_3/main.js
@@ -15,6 +15,8 @@ EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 
 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.*/
 
+
+// a. заповнити його 50 парними числами за допомоги циклу.
 let emptyArr = [];
 for (let i = 0; i < 100; i++) {
     if (i % 2 === 0) {
@@ -26,6 +28,7 @@ for (let i = 0; i < 100; i++) {
 
 document.write("-----------------------------------------------------");
 
+// b. заповнити його 50 непарними числами за допомоги циклу.
 let emptyArr1 = [];
 for (let i = 0; i < 100; i++) {
     if (i % 2 !== 0) {
@@ -35,28 +38,84 @@ for (let i = 0; i < 100; i++) {
 
 }
 
+// c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
 let randomArr = [];
 for (let i = 0; i < 20; i++) {
     randomArr[i] = Math.floor(Math.random() * 10);
     document.write(`<div> ${randomArr[i]} </div>`);
 }
 
-console.log("----------------------------------------------------------")
-
+//d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
 let randomArr1 = [];
 for (let i = 0; i < 20; i++) {
-    randomArr[i] = Math.floor(Math.random() * (732 - 8)) + 8;
-    document.write(`<div> ${randomArr[i]} </div>`);
+    randomArr1[i] = Math.floor(Math.random() * (732 - 8)) + 8;
+    document.write(`<div> ${randomArr1[i]} </div>`);
 }
 
 
+//2. Вивести за допомогою console.log кожен третій елемен
+console.log(randomArr1);
+for (let i = 0; i < randomArr1.length; i += 3) {
+    console.log(randomArr1[i]);
+}
 
 
+// 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
+let newArr = [];
+let j = 0;
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 234, 27654, 354, 645, 78, 6234238, 798, 6786, 68, 67, 23, 4234, 1, 41246, 45, 675, 7, 5687, 345, 6757, 5, 675, 75, 75, 7, 43, 563, 456, 547, 54, 67, 36, 346];
 
+for (let i = 0; i < arr.length; i += 2) {
+    if (arr[i] % 2 === 0) {
+        newArr[j] = arr[i];
+        j++;
+    }
+}
+console.log(newArr);
 
+//5. Вивести кожен елемент масиву, сусід справа якого є парним
+// EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
 
+let exampleArr = [1, 2, 3, 5, 7, 9, 56, 8, 67];
 
+for (let i = 0; i < exampleArr.length; i++) {
+    if (exampleArr[i + 1] % 2 === 0) {
+        console.log(exampleArr[i]);
+    }
+}
 
+//6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
 
+let priceArr = [100, 250, 50, 168, 120, 345, 188];
+let sum = 0;
 
+for (let i = 0; i < priceArr.length; i++) {
+    sum += priceArr[i];
+}
+let result = sum / priceArr.length;
+console.log(result);
+
+//7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+
+let arrSevenTask = [];
+let arrSevenTaskMul = [];
+for (let i = 0; i < 10; i++) {
+    arrSevenTask[i] = Math.floor(Math.random() * 100);
+    arrSevenTaskMul[i] = arrSevenTask[i] * 5;
+}
+console.log(arrSevenTask);
+console.log(arrSevenTaskMul);
+
+// 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.*/
+let mixArr = [1, 2, 3, 4, 5, 1, 2, "a", "b", false, true, "a", "b", "c", "d", "e",234,456,547,567,234];
+let numberArr=[];
+let k=0;
+
+for (let i=0; i< mixArr.length; i++){
+    if (typeof mixArr[i]==="number"){
+        numberArr[k] = mixArr[i];
+        k++;
+    }
+}
+console.log(numberArr);
 
