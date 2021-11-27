44e141c0c253b2e96ca1b23c1dc604949a0f7184
RomanBuryy <roman.buriy@gmail.com>
2021-11-27 20:24:20 +0200

    Add homework lesson_7

--- lesson_7/classwork/main.js
+++ lesson_7/classwork/main.js
@@ -16,29 +16,43 @@ function Cars(model, manufacturer, year, maxSpeed, engineCapacity) {
     this.engineCapacity = engineCapacity;
 
     this.drive = function () {
-        console.log(`їдемо зі швидкістю ${maxSpeed} на годину`);
-    }
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+    };
 
 
     this.info = function () {
-        console.log("Модель - " + model);
-        console.log("Виробник - " + manufacturer);
-        console.log("Рік - " + year);
-        console.log("Максимальна швидкість - " + maxSpeed);
-        console.log("Об'єм двигуна - " + engineCapacity);
-    }
+        console.log("Модель - " + this.model);
+        console.log("Виробник - " + this.manufacturer);
+        console.log("Рік - " + this.year);
+        console.log("Максимальна швидкість - " + this.maxSpeed);
+        console.log("Об'єм двигуна - " + this.engineCapacity);
+
+    };
 
     this.increaseMaxSpeed = function (newSpeed) {
-        maxSpeed = newSpeed;
-    }
+        this.maxSpeed = newSpeed;
+    };
 
+    this.changeYear = function (newValue) {
+        this.year = newValue;
+    };
 
+    this.addDriver = function (driver) {
+        this.driver = driver
+    };
 }
 
 let car = new Cars(1, 2, 3, 4, 5);
+
 car.info();
 car.increaseMaxSpeed(400);
+car.changeYear(2014);
+car.addDriver({"driverName": "Roman", "driverAge": 37});
 car.info();
+console.log(car);
+
+
+console.log("---------------------------------------------------------------");
 
 /*
 - (Те саме, тільки через клас)
@@ -49,9 +63,126 @@ car.info();
 -- changeYear (newValue) - змінює рік випуску на значення newValue
 -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car*/
 
+class newCars {
+    constructor(model, manufacturer, year, maxSpeed, engineCapacity) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.maxSpeed = maxSpeed;
+        this.engineCapacity = engineCapacity;
+        this.year = year;
+    }
+
+    drive() {
+        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+    }
+
+    info() {
+        console.log("Модель - " + this.model);
+        console.log("Виробник - " + this.manufacturer);
+        console.log("Рік - " + this.year);
+        console.log("Максимальна швидкість - " + this.maxSpeed);
+        console.log("Об'єм двигуна - " + this.engineCapacity);
+    }
+
+    increaseMaxSpeed(newSpeed) {
+        this.maxSpeed = newSpeed;
+    }
+
+    changeYear (newValue){
+        this.year = newValue;
+    }
+
+    addDriver (driver){
+        this.driver = driver;
+    }
+
+}
+
+
+let newCar = new newCars(1, 2, 3, 4, 5);
+newCar.drive();
+newCar.info();
+newCar.increaseMaxSpeed(400);
+newCar.info();
+newCar.changeYear(2021);
+newCar.info();
+newCar.addDriver({"driverName": "Roman", "driverAge": 37});
+console.log(newCar);
+
 
 /*
 -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
 Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку*/
+
+class Cinderella {
+    constructor(name, age, footSize) {
+        this.name = name;
+        this.age = age;
+        this.footSize = footSize;
+    }
+}
+
+class Prince {
+    constructor(name, age, shoeSize) {
+        this.name = name;
+        this.age = age;
+        this.shoeSize = shoeSize;
+    }
+}
+
+let girl1 = new Cinderella("Kathryn", 23, 30);
+let girl2 = new Cinderella("Fern", 23, 31);
+let girl3 = new Cinderella("Rita", 23, 31);
+let girl4 = new Cinderella("Roxanne", 23, 34);
+let girl5 = new Cinderella("Alannah", 23, 40);
+let girl6 = new Cinderella("Constance", 23, 27);
+let girl7 = new Cinderella("Lee", 23, 45);
+let girl8 = new Cinderella("Tanya", 23, 44);
+let girl9 = new Cinderella("Melanie", 23, 40);
+let girl10 = new Cinderella("Mollie", 23, 46);
+
+let prince = new Prince("Roman", 37, 45);
+let girls = [girl1, girl2, girl3, girl4, girl5, girl6, girl7, girl8, girl9, girl10];
+
+
+let princessFound = girls.find((girl) => girl.footSize===prince.shoeSize);
+
+if (princessFound){
+    console.log(`We found princes - his name is ${princessFound.name}. She is ${princessFound.age} years old`);
+} else {
+    console.log("Sorry, but we didn't find the princess");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
