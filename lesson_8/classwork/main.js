48ae2d82dc5eba379da20cf168162dc5b8e93e51
RomanBuryy <roman.buriy@gmail.com>
2021-12-01 19:48:15 +0200

    Add classwork lesson_8

--- lesson_8/classwork/main.js
+++ lesson_8/classwork/main.js
@@ -45,7 +45,7 @@ for (const addClassToLinkElement of addClassToLink) {
 let changeText = document.getElementsByTagName('a');
 
 for (const changeTextElement of changeText) {
-    if (changeTextElement.innerText === 'link3'){
+    if (changeTextElement.innerText === 'link3') {
         changeTextElement.style.fontSize = '40px';
     }
 }
@@ -60,45 +60,40 @@ for (const addClassWithContentElement of addClassWithContent) {
 
 // i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
 
-// let sub_headers = document.getElementsByClassName('sub-header');
-//
-// for (const subHeader of sub_headers) {
-//     subHeader.style.background = prompt('Enter color: ');
-// }
+let sub_headers = document.getElementsByClassName('sub-header');
+
+for (const subHeader of sub_headers) {
+    subHeader.style.background = prompt('Enter color: ');
+}
 
 // j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
 let subHeaders = document.getElementsByClassName('sub-header');
 
-for (subHeader of subHeaders){
-    if(subHeader.innerText === 'content 2 segment'){
-        subHeader.style.color = prompt('Enter color for subHeaders ');
+for (subHeader of subHeaders) {
+    if (subHeader.innerText === 'content 2 segment') {
+        //subHeader.style.color = prompt('Enter color for subHeaders ');
     }
 }
 
 
 // k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
-
 let content_1 = document.getElementsByClassName('content_1');
 
-for (cont of content_1){
+for (const content1Element of content_1) {
+    content1Element.innerText = prompt('Enter content_1 text: ');
+}
 
+// l) отримати елементи p та змінити їм padding на 20px
+let tagP = document.body.getElementsByTagName('p');
+
+for (const tagPElement of tagP) {
+    tagPElement.style.padding = '20px';
 }
 
 
-
-
-
-
-
-
-// l) отримати елементи p та змінити їм padding на 20px
-
-
-
-
-
-
-
-
-
 // m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
+let classText2 = document.getElementsByClassName('text2');
+
+for (const classText2Element of classText2) {
+    classText2Element.innerText = 'sp-2021';
+}
