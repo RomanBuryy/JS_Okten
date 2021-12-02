08c46194282940c4df40f89eeae30a4d98432ed3
RomanBuryy <roman.buriy@gmail.com>
2021-12-02 21:50:38 +0200

    add classwork task_9

--- lesson_9/homework/main.js
+++ lesson_9/homework/main.js
@@ -7,7 +7,7 @@
 //
 
 let div= document.createElement('div');
-div.classList.add( 'wrap', 'collapse', 'alpha', 'beta');
+div.classList.add( 'wradp', 'collapse', 'alpha', 'beta');
 div.style.color='green';
 div.style.backgroundColor='aqua';
 div.style.fontSize = '20px';
@@ -27,7 +27,11 @@ document.body.appendChild(divClone);
 
 let arr = ['Main','Products','About us','Contacts'];
 
-
+for (let i=0; i<arr.length; i++){
+    let li = document.createElement('li');
+    li.innerText = arr[i];
+    document.getElementsByClassName('menu')[0].appendChild(li);
+}
 
 
 
@@ -44,6 +48,42 @@ let arr = ['Main','Products','About us','Contacts'];
 // Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
 // Завдання робити через цикли.
 //
+
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+let divCourses = document.createElement('div');
+let coursesUl = document.createElement('ul');
+divCourses.classList.add('wrap');
+coursesUl.classList.add('courses');
+divCourses.appendChild(coursesUl);
+
+document.body.appendChild(divCourses);
+
+for (const course in coursesAndDurationArray ) {
+    let li = document.createElement('li');
+
+    li.innerText = course.title;
+    document.getElementsByClassName('courses')[0].appendChild(li);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
