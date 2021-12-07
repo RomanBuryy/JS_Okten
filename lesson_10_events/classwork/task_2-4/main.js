7bb238ac9be7325c71dadc9bd0bb81978cfca7da
RomanBuryy <roman.buriy@gmail.com>
2021-12-07 14:34:10 +0200

    Add classwork lesson_10

--- lesson_10_events/classwork/task_2-4/main.js
+++ lesson_10_events/classwork/task_2-4/main.js
@@ -3,7 +3,7 @@
 
 let table = document.createElement('table');
 let f1 = document.forms.form1;
-let btn = document.getElementById('btn');
+
 document.body.appendChild(table);
 f1.onsubmit = function (e) {
     e.preventDefault();
@@ -20,16 +20,52 @@ f1.onsubmit = function (e) {
 }
 
 
-// (Додатковачастина для завдання)
+// (Додатковачастина для завдання)1
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Якщо людина вводить слово і воно міститься в масиві не цензурних слів
 // кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
-//
-//
+
+let censoredWords = ['foo', 'boo', 'too'];
+let div = document.createElement('div');
+let f2 = document.forms.form2;
+let btn = document.getElementById('checkWord');
+
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    for (let i = 0; i < censoredWords.length; i++) {
+        if (censoredWords[i] === f2.word.value) {
+            return alert('You entered censored word!!!');
+        }
+
+
+    }
+    return alert('You entered correct word!!!');
+
+
+});
+
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
+
+let div2 = document.createElement('div');
+let f3 = document.forms.form3;
+let btn2 = document.getElementById('checkString');
+
+btn2.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    for (const str of f3.word1.value.split(' ')) {
+        for (let i = 0; i < censoredWords.length; i++) {
+            if (censoredWords[i] === str) {
+                return alert('You entered censored word!!!');
+            }
+        }
+    }
+    return alert('You entered correct STRING!!!');
+});
