1d8032648d0c3d1bd8ce8e83a7064b9799a35f09
RomanBuryy <roman.buriy@gmail.com>
2021-12-07 19:26:11 +0200

    add additional task_10

---
+++ lesson_10_events/additional/main.js
@@ -0,0 +1,43 @@
+// -- взять массив пользователей
+let usersWithAddress = [
+    {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+    {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+    {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+    {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+    {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+    {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+    {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+    {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+    {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id: 11, name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+];
+// - Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
+// 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
+// 2й - оставляет старше 29 лет включительно
+// 3й - оставляет тех у кого город киев
+// Данные выводить в документ
+
+let checkedFalse = document.getElementById('false');
+let checkedAge = document.getElementById('age');
+let newArr = [];
+function checkboxFalse() {
+    if (checkedFalse.checked) {
+        newArr = usersWithAddress.filter(value => value.status === false);
+        console.log(newArr);
+    } else {
+        console.log(usersWithAddress);
+    }
+}
+
+function checkedAgeFunc(){
+    if (checkedAge.checked) {
+        let arr = newArr.filter(value => value.age >=29);
+        console.log(arr);
+    } else if (!checkedAge.checked) {
+        console.log(newArr);
+    }
+}
+
+checkedFalse.addEventListener('change', checkboxFalse);
+checkedAge.addEventListener('change', checkedAgeFunc);
