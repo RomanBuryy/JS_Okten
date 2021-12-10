a20e6b207902bfafb65d4ed5df25180196e5d355
RomanBuryy <roman.buriy@gmail.com>
2021-12-11 00:26:35 +0200

    add task lesson 11

--- lesson_11/homework/main.js
+++ lesson_11/homework/main.js
@@ -17,13 +17,19 @@ formUser.onsubmit = () => {
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
 
 let carInfo = document.forms.carInfo;
+localStorage.setItem('Car', JSON.stringify(['Mers', 'hetch', 2.2]));
 
 carInfo.onsubmit = (e) => {
     e.preventDefault();
     let carModel = carInfo.model.value;
     let carType = carInfo.type.value;
     let carVolume = carInfo.volume.value;
+    let car = [carModel, carType, carVolume];
 
-    localStorage.setItem('Car', JSON.stringify([carModel, carType, carVolume]));
+    let res = localStorage.getItem('Car');
+    let cars = JSON.parse(res);
+    cars.push(car);
+
+    localStorage.setItem('Car', JSON.stringify(cars));
 }
 
